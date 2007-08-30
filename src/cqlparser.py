826f36e8733fe6960d5f3ce97fa0aec7f193d07f
KvS <KvS@7f82bda0-831b-0410-895c-c142f06d1e64>
2007-08-30 13:17:20 +0000

    KVS: Optimizations for parser (look ahead and less rollbacks)
    git-svn-id: file:///vol/seecrbackup/svn/backup-20130318-0300/cqlparser/trunk@5650 7f82bda0-831b-0410-895c-c142f06d1e64

--- src/cqlparser.py
+++ src/cqlparser.py
@@ -23,207 +23,225 @@
 ## end license ##
 
 class UnsupportedCQL(Exception):
-	def __call__(self, *ignoredArgs):
-		raise self
+    def __call__(self, *ignoredArgs):
+        raise self
 
 class CQLParseException(Exception):
-	pass
+    pass
 
 class CQLAbstractSyntaxNode:
 
-	def __init__(self, *args):
-		self._children = args
-		self.__repr__ = self.__str__
+    def __init__(self, *args):
+        self._children = args
+        self.__repr__ = self.__str__
 
-	def __str__(self):
-		return "%s(%s)" % (self.__class__, ", ".join(map(repr, self._children)))
+    def __str__(self):
+        return "%s(%s)" % (self.__class__, ", ".join(map(repr, self._children)))
 
-	def __eq__(self, other):
-		return self.__class__ == other.__class__ and self._children == other._children
-	
-	def children(self):
-		return self._children
-	
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and self._children == other._children
+    
+    def children(self):
+        return self._children
+    
 for aClass in ['CQL_QUERY', 'SCOPED_CLAUSE', 'BOOLEAN', 'SEARCH_CLAUSE', 'SEARCH_TERM', 'INDEX', 'COMPARITOR']:
-	exec("""class %s(CQLAbstractSyntaxNode):
-	pass""" % aClass)
+    exec("""class %s(CQLAbstractSyntaxNode):
+    pass""" % aClass)
 
 def parseString(cqlString):
-	from cqltokenizer import tokenStack
-	parser = CQLParser(tokenStack(cqlString))
-	return parser.parse()
+    from cqltokenizer import tokenStack
+    parser = CQLParser(tokenStack(cqlString))
+    return parser.parse()
 
 class Token:
-	def __init__(self, parser, token, caseSensitive = True):
-		self._parser = parser
-		self._token = token
-		self._caseSensitive = caseSensitive
-	
-	def __call__(self):
-		#TODO refactor
-		nextToken = self._parser._tokens.safeNext()
-		if not nextToken:
-			return False
-		
-		if not self._caseSensitive:
-			return nextToken.lower() == self._token.lower() and self._token.lower()
-		if nextToken == self._token:
-			return self._token
-		return False
-	
+    def __init__(self, parser, token, caseSensitive = True):
+        self._parser = parser
+        self._token = token
+        self._caseSensitive = caseSensitive
+    
+    def __call__(self):
+        #TODO refactor
+        nextToken = self._parser._tokens.safeNext()
+        if not nextToken:
+            return False
+        
+        if not self._caseSensitive:
+            return nextToken.lower() == self._token.lower() and self._token.lower()
+        if nextToken == self._token:
+            return self._token
+        return False
+    
 class CQLParser:
-	def __init__(self, tokenstack):
-		self._tokens = tokenstack
-		for term in ['prefix', 'uri', 'modifierName', 'modifierValue']:
-			setattr(self, term, self.term)
-			#(hoewel hier eigenlijk nog een veralgemeniseerde wrap laag omheen zou kunnen)
+    def __init__(self, tokenstack):
+        self._tokens = tokenstack
+        for term in ['prefix', 'uri', 'modifierName', 'modifierValue']:
+            setattr(self, term, self.term)
+            #(hoewel hier eigenlijk nog een veralgemeniseerde wrap laag omheen zou kunnen)
 
-	def construct(self, constructor, *termFunctions):
-		result = []
-		self._tokens.bookmark()
-		for termFunction in termFunctions:
-			term = termFunction()
-			if not term:
-				self._tokens.revertToBookmark()
-				return False
-			result.append(term)
-		self._tokens.dropBookmark()
-		return constructor(*result)
+    def tryTerms(self, *termFunctions):
+        result = []
+        self._tokens.bookmark()
+        for termFunction in termFunctions:
+            term = termFunction()
+            if not term:
+                self._tokens.revertToBookmark()
+                return False
+            result.append(term)
+        self._tokens.dropBookmark()
+        return result
+    
+    def construct(self, constructor, *termFunctions):
+        result = self.tryTerms(*termFunctions)
+        if not result:
+            return False
+        return constructor(*result)
 
-	def term(self):
-		token = self._tokens.safeNext()
-		if token and (not token[:1] in ['(', ')', '>', '=', '<', '/']):
-			return token
-		return False
+    def term(self):
+        token = self._tokens.safeNext()
+        if token and (not token[:1] in ['(', ')', '>', '=', '<', '/']):
+            return token
+        return False
 
-	def searchTerm(self):
-		return self.construct(SEARCH_TERM, self.term)
+    def searchTerm(self):
+        return self.construct(SEARCH_TERM, self.term)
 
-	def index(self):
-		"""index ::= term"""
-		return self.construct(INDEX, self.term)
+    def index(self):
+        """index ::= term"""
+        return self.construct(INDEX, self.term)
 
-	def token(self, aToken, caseSensitive = True):
-		return Token(self, aToken, caseSensitive)
-	
-	def parse(self):
-		result = self.cqlQuery()
-		if self._tokens.hasNext():
-			raise CQLParseException('Unexpected token after parsing, check for greediness ([%s], %s).' % (self._tokens.next(), str(result)))
-		return result
+    def token(self, aToken, caseSensitive = True):
+        return Token(self, aToken, caseSensitive)
+    
+    def parse(self):
+        result = self.cqlQuery()
+        if self._tokens.hasNext():
+            raise CQLParseException('Unexpected token after parsing, check parser for greediness ([%s], %s).' % (self._tokens.next(), str(result)))
+        return result
 
-	def cqlQuery(self):
-		"""cqlQuery ::= prefixAssignment cqlQuery | scopedClause"""
-		return \
-			self.construct(CQL_QUERY,
-				self.prefixAssignment,
-				self.cqlQuery) or \
-			self.construct(CQL_QUERY,
-				self.scopedClause)	
+    def cqlQuery(self):
+        """cqlQuery ::= prefixAssignment cqlQuery | scopedClause"""
+        if self._tokens.hasNext() and self._tokens.peek() == ">":
+            return \
+                self.construct(CQL_QUERY,
+                    self.prefixAssignment,
+                    self.cqlQuery)
+        return \
+            self.construct(CQL_QUERY,
+                self.scopedClause)	
 
-	def prefixAssignment(self):
-		"""prefixAssignment ::= '>' prefix '=' uri | '>' uri"""
-		return \
-			self.construct(UnsupportedCQL("prefixAssignment (>)"),
-				self.token('>'),
-				self.prefix,
-				self.token('='),
-				self.uri) or \
-			self.construct(UnsupportedCQL("prefixAssignment (>)"),
-				self.token('>'),
-				self.uri)
-				
-	def scopedClause(self):
-		"""
-		scopedClause ::= scopedClause booleanGroup searchClause | searchClause
-		we use:
-		scopedClause ::= searchClause booleanGroup scopedClause | searchClause
-		"""
-		return \
-			self.construct(SCOPED_CLAUSE,
-				self.searchClause,
-				self.booleanGroup,
-				self.scopedClause) or \
-			self.construct(SCOPED_CLAUSE,
-				self.searchClause)
-	
-	def boolean(self):
-		"""boolean ::= 'and' | 'or' | 'not' | 'prox'"""
-		return \
-			self.construct(UnsupportedCQL("booleanGroup: 'prox'"),
-				self.token('prox')) or \
-			self.construct(BOOLEAN, self.token('and', caseSensitive = False)) or \
-			self.construct(BOOLEAN, self.token('or', caseSensitive = False)) or \
-			self.construct(BOOLEAN, self.token('not', caseSensitive = False))
-	
-	def booleanGroup(self):
-		"""
-		booleanGroup ::= boolean [ modifierList ]
-		we use:
-		booleanGroup ::= boolean modifierList | boolean
-		"""
-		return \
-			self.construct(UnsupportedCQL("modifierLists are not supported"),
-				self.boolean,
-				self.modifierList) or \
-			self.construct(lambda x: x,
-				self.boolean)
-	
-	def modifierList(self):
-		return self.token('/')()
+    def prefixAssignment(self):
+        """prefixAssignment ::= '>' prefix '=' uri | '>' uri"""
+        return \
+            self.construct(UnsupportedCQL("prefixAssignment (>)"),
+                self.token('>'),
+                self.prefix,
+                self.token('='),
+                self.uri) or \
+            self.construct(UnsupportedCQL("prefixAssignment (>)"),
+                self.token('>'),
+                self.uri)
+                
+    def scopedClause(self):
+        """
+        scopedClause ::= scopedClause booleanGroup searchClause | searchClause
+        we use:
+        scopedClause ::= searchClause booleanGroup scopedClause | searchClause
+        """
+        #searchClause = self.searchClause()
+        
+        head = self.tryTerms(self.searchClause)
+        if not head:
+            return False
+        tail = self.tryTerms(
+                self.booleanGroup,
+                self.scopedClause)
+        if tail:
+            return SCOPED_CLAUSE(*(head + tail))
+        return SCOPED_CLAUSE(*head)
+    
+    def boolean(self):
+        """boolean ::= 'and' | 'or' | 'not' | 'prox'"""
+        return \
+            self.construct(UnsupportedCQL("booleanGroup: 'prox'"),
+                self.token('prox')) or \
+            self.construct(BOOLEAN, self.token('and', caseSensitive = False)) or \
+            self.construct(BOOLEAN, self.token('or', caseSensitive = False)) or \
+            self.construct(BOOLEAN, self.token('not', caseSensitive = False))
+    
+    def booleanGroup(self):
+        """
+        booleanGroup ::= boolean [ modifierList ]
+        we use:
+        booleanGroup ::= boolean modifierList | boolean
+        """
+        return \
+            self.construct(UnsupportedCQL("modifierLists are not supported"),
+                self.boolean,
+                self.modifierList) or \
+            self.construct(lambda x: x,
+                self.boolean)
+    
+    def modifierList(self):
+        return self.token('/')()
 
-	def searchClause(self):
-		"""
-		searchClause ::=
-			'(' cqlQuery ')' | 
-			index relation searchTerm |
-			searchTerm
-		"""
-		return \
-			self.construct(SEARCH_CLAUSE,
-				self.token('('),
-				self.cqlQuery,
-				self.token(')')) or \
-			self.construct(SEARCH_CLAUSE,
-				self.index,
-				self.relation,
-				self.searchTerm) or \
-			self.construct(SEARCH_CLAUSE,
-				self.searchTerm)
-				
-	def relation(self):
-		"""
-		relation ::= comparitor [modifierList]
-		we use:
-		relation ::= comparitor modifierList | comparitor
-		"""
-		return \
-			self.construct(UnsupportedCQL("modifierLists are not supported"),
-				self.comparitor,
-				self.modifierList) or \
-			self.construct(lambda x: x,
-				self.comparitor)
-		
-	def comparitor(self):
-		"""
-		comparitor ::= comparitorSymbol | namedComparitor
-		comparitorSymbol ::= '=' | '>' | '<' | '>=' | '<=' | '<>'
-		we use a shortcut since most of this is not supported
-		"""
-		return \
-			self.construct(COMPARITOR,
-				self.token('=')) or \
-			self.construct(UnsupportedCQL("Unsupported Relation: >"),
-				self.token('>')) or \
-			self.construct(UnsupportedCQL("Unsupported Relation: <"),
-				self.token('<')) or \
-			self.construct(UnsupportedCQL("Unsupported Relation: >="),
-				self.token('>=')) or \
-			self.construct(UnsupportedCQL("Unsupported Relation: <="),
-				self.token('<=')) or \
-			self.construct(UnsupportedCQL("Unsupported Relation: <>"),
-				self.token('<>'))
-			#this needs to be unparsable, not throw an exception.
-			#self.construct(UnsupportedCQL("Unsupported Relation: namedComparitor"),
-			#	self.term)
-		
+    def searchClause(self):
+        """
+        searchClause ::=
+            '(' cqlQuery ')' | 
+            index relation searchTerm |
+            searchTerm
+        """
+        if self._tokens.hasNext() and self._tokens.peek() == "(":
+            return \
+                self.construct(SEARCH_CLAUSE,
+                    self.token('('),
+                    self.cqlQuery,
+                    self.token(')'))
+        head = self.tryTerms(self.term)
+        if not head:
+            return False
+        tail = self.tryTerms(
+                self.relation,
+                self.searchTerm)
+        if tail:
+            return SEARCH_CLAUSE(*([INDEX(term) for term in head] + tail))
+        return SEARCH_CLAUSE(*[SEARCH_TERM(term) for term in head])
+                
+    def relation(self):
+        """
+        relation ::= comparitor [modifierList]
+        we use:
+        relation ::= comparitor modifierList | comparitor
+        """
+        if self._tokens.hasNext() and self._tokens.peek() == "/":
+            return \
+                self.construct(UnsupportedCQL("modifierLists are not supported"),
+                    self.comparitor,
+                    self.modifierList)
+        return \
+            self.construct(lambda x: x,
+                self.comparitor)
+        
+    def comparitor(self):
+        """
+        comparitor ::= comparitorSymbol | namedComparitor
+        comparitorSymbol ::= '=' | '>' | '<' | '>=' | '<=' | '<>'
+        we use a shortcut since most of this is not supported
+        """
+        return \
+            self.construct(COMPARITOR,
+                self.token('=')) or \
+            self.construct(UnsupportedCQL("Unsupported Relation: >"),
+                self.token('>')) or \
+            self.construct(UnsupportedCQL("Unsupported Relation: <"),
+                self.token('<')) or \
+            self.construct(UnsupportedCQL("Unsupported Relation: >="),
+                self.token('>=')) or \
+            self.construct(UnsupportedCQL("Unsupported Relation: <="),
+                self.token('<=')) or \
+            self.construct(UnsupportedCQL("Unsupported Relation: <>"),
+                self.token('<>'))
+            #this needs to be unparsable, not throw an exception.
+            #self.construct(UnsupportedCQL("Unsupported Relation: namedComparitor"),
+            #	self.term)
+        
--- test/cqlparsertest.py
+++ test/cqlparsertest.py
@@ -25,69 +25,68 @@
 import unittest
 
 from cqlparser import CQLParser, parseString, \
-	CQL_QUERY, SCOPED_CLAUSE, SEARCH_CLAUSE, BOOLEAN, SEARCH_TERM, INDEX, COMPARITOR, UnsupportedCQL, CQLParseException
+    CQL_QUERY, SCOPED_CLAUSE, SEARCH_CLAUSE, BOOLEAN, SEARCH_TERM, INDEX, COMPARITOR, UnsupportedCQL, CQLParseException
 
 class CQLParserTest(unittest.TestCase):
-	
-	def testOneTerm(self):
-		self.assertEquals(CQL_QUERY(SCOPED_CLAUSE(SEARCH_CLAUSE(SEARCH_TERM('term')))), parseString('term'))
-		self.assertEquals(CQL_QUERY(SCOPED_CLAUSE(SEARCH_CLAUSE(SEARCH_TERM('"white space"')))), parseString('"white space"'))
-		
-	def testTwoTerms(self):
-		self.assertEquals(
-			CQL_QUERY(SCOPED_CLAUSE(
-				SEARCH_CLAUSE(SEARCH_TERM('term')), BOOLEAN('and'), 
-					SCOPED_CLAUSE(SEARCH_CLAUSE(SEARCH_TERM('term2'))))),
-			parseString('term and term2'))
-		
-	def testBooleansAreCaseInsensitive(self):
-		self.assertEquals(
-			CQL_QUERY(SCOPED_CLAUSE(
-				SEARCH_CLAUSE(SEARCH_TERM('term')), BOOLEAN('and'), 
-					SCOPED_CLAUSE(SEARCH_CLAUSE(SEARCH_TERM('term2'))))),
-			parseString('term AnD term2'))
-		
-	def testPrefixesAreIllegal(self):
-		self.assertException(UnsupportedCQL, "> prefix = some:thing aquery")
+    
+    def testOneTerm(self):
+        self.assertEquals(CQL_QUERY(SCOPED_CLAUSE(SEARCH_CLAUSE(SEARCH_TERM('term')))), parseString('term'))
+        self.assertEquals(CQL_QUERY(SCOPED_CLAUSE(SEARCH_CLAUSE(SEARCH_TERM('"white space"')))), parseString('"white space"'))
+        
+    def testTwoTerms(self):
+        self.assertEquals(
+            CQL_QUERY(SCOPED_CLAUSE(
+                SEARCH_CLAUSE(SEARCH_TERM('term')), BOOLEAN('and'), 
+                    SCOPED_CLAUSE(SEARCH_CLAUSE(SEARCH_TERM('term2'))))),
+            parseString('term and term2'))
+        
+    def testBooleansAreCaseInsensitive(self):
+        self.assertEquals(
+            CQL_QUERY(SCOPED_CLAUSE(
+                SEARCH_CLAUSE(SEARCH_TERM('term')), BOOLEAN('and'), 
+                    SCOPED_CLAUSE(SEARCH_CLAUSE(SEARCH_TERM('term2'))))),
+            parseString('term AnD term2'))
+        
+    def testPrefixesAreIllegal(self):
+        self.assertException(UnsupportedCQL, "> prefix = some:thing aquery")
 
-	def testUnfinishedQuery(self):
-		self.assertException(CQLParseException, 'term and')
-	
-	def testIllegalBooleanGroups(self):
-		self.assertException(CQLParseException, 'term notanyof_and_or_not_prox term2')
-		self.assertException(UnsupportedCQL, 'term prox term2')
-		self.assertException(UnsupportedCQL, 'term and / aModifierName term2')
-		
-	def testParentheses(self):
-		Q = CQL_QUERY
-		SC = SCOPED_CLAUSE
-		SE = SEARCH_CLAUSE
-		T = SEARCH_TERM
-		self.assertEquals(Q(SC(SE("(", Q(SC(SE(T('term')))), ")"))), parseString('(term)'))
-		self.assertEquals(Q(SC(SE("(", Q(SC(SE("(", Q(SC(SE(T('term')))), ")"))), ")"))), parseString('((term))'))
-		
-		self.assertEquals(Q(SC(SE("(", Q(SC(SE(T('term')), BOOLEAN('and'), SC(SE(T('term2'))))), ")"))), parseString('(term and term2)'))
-		
-		self.assertException(CQLParseException, '(term')
-		self.assertException(CQLParseException, '(term term2')
-	
-	def testTermsAreIdentifiers(self):
-		self.assertException(CQLParseException, ')')
-		
-	def testIndexRelationSearchTerm(self):
-		Q = CQL_QUERY
-		SC = SCOPED_CLAUSE
-		SE = SEARCH_CLAUSE
-		T = SEARCH_TERM
-		self.assertEquals(Q(SC(SE(INDEX('field1'), COMPARITOR('='), T('200')))), parseString('field1 = 200'))
-		for comparitor in ['>', '<', '>=', '<=', '<>']:
-			self.assertException(UnsupportedCQL, 'field1 %s 200' % comparitor)
-		self.assertException(UnsupportedCQL, 'field1 = /aModifierName 200')
-	
-	### Helper methods
-	def assertException(self, exceptionClass, queryString):
-		try:
-			parseString(queryString)
-			self.fail()
-		except exceptionClass, e:
-			pass
+    def testUnfinishedQuery(self):
+        self.assertException(CQLParseException, 'term and')
+    
+    def testIllegalBooleanGroups(self):
+        self.assertException(CQLParseException, 'term notanyof_and_or_not_prox term2')
+        self.assertException(UnsupportedCQL, 'term prox term2')
+        self.assertException(UnsupportedCQL, 'term and / aModifierName term2')
+        
+    def testParentheses(self):
+        Q = CQL_QUERY
+        SC = SCOPED_CLAUSE
+        SE = SEARCH_CLAUSE
+        T = SEARCH_TERM
+        self.assertEquals(Q(SC(SE("(", Q(SC(SE(T('term')))), ")"))), parseString('(term)'))
+        self.assertEquals(Q(SC(SE("(", Q(SC(SE("(", Q(SC(SE(T('term')))), ")"))), ")"))), parseString('((term))'))
+        
+        self.assertEquals(Q(SC(SE("(", Q(SC(SE(T('term')), BOOLEAN('and'), SC(SE(T('term2'))))), ")"))), parseString('(term and term2)'))
+        
+        self.assertException(CQLParseException, '(term')
+        self.assertException(CQLParseException, '(term term2')
+    
+    def testTermsAreIdentifiers(self):
+        self.assertException(CQLParseException, ')')
+        
+    def testIndexRelationSearchTerm(self):
+        Q = CQL_QUERY
+        SC = SCOPED_CLAUSE
+        SE = SEARCH_CLAUSE
+        T = SEARCH_TERM
+        self.assertEquals(Q(SC(SE(INDEX('field1'), COMPARITOR('='), T('200')))), parseString('field1 = 200'))
+        for comparitor in ['>', '<', '>=', '<=', '<>']:
+            self.assertException(UnsupportedCQL, 'field1 %s 200' % comparitor)
+    
+    ### Helper methods
+    def assertException(self, exceptionClass, queryString):
+        try:
+            parseString(queryString)
+            self.fail()
+        except exceptionClass, e:
+            pass
