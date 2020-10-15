d02c1c1130adb14b0b8fac4393f561a3bcbc38ec
Thijs Janssen <thijs@seecr.nl>
2020-10-15 10:25:24 +0200

    TJ: license.

--- cqlparser/_cqlexception.py
+++ cqlparser/_cqlexception.py
@@ -2,7 +2,7 @@
 #
 # "CQLParser" is a parser that builds a parsetree for the given CQL and can convert this into other formats.
 #
-# Copyright (C) 2016 Seecr (Seek You Too B.V.) https://seecr.nl
+# Copyright (C) 2016, 2020 Seecr (Seek You Too B.V.) https://seecr.nl
 # Copyright (C) 2016 Stichting Kennisnet https://www.kennisnet.nl
 #
 # This file is part of "CQLParser"
--- test/_alltests.py
+++ test/_alltests.py
@@ -3,7 +3,7 @@
 # "CQLParser" is a parser that builds a parsetree for the given CQL and can convert this into other formats.
 #
 # Copyright (C) 2005-2010 Seek You Too (CQ2) http://www.cq2.nl
-# Copyright (C) 2012-2013, 2015 Seecr (Seek You Too B.V.) https://seecr.nl
+# Copyright (C) 2012-2013, 2015, 2020 Seecr (Seek You Too B.V.) https://seecr.nl
 # Copyright (C) 2015 Stichting Kennisnet https://www.kennisnet.nl
 #
 # This file is part of "CQLParser"
