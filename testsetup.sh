87e36c8fa321d2c6830831e42153b0898d61f08e
Thijs Janssen <thijs@seecr.nl>
2021-01-08 14:34:23 +0100

    TJ: testsetup.

--- testsetup.sh
+++ testsetup.sh
@@ -31,21 +31,12 @@ rm -rf tmp build
 mydir=$(cd $(dirname $0); pwd)
 source /usr/share/seecr-tools/functions.d/test
 
-pyversions=""
-if [ -e /usr/bin/python2.6 ]; then
-    pyversions="python2.6"
-fi
-if [ -e /usr/bin/python2.7 ]; then
-    pyversions="${pyversions} python2.7"
-fi
-
 VERSION="x.y.z"
 
-for pyversion in $pyversions; do
-    definePythonVars $pyversion
-    echo "###### $pyversion, $PYTHON"
-    ${PYTHON} setup.py install --root tmp
-done
+definePythonVars
+echo "Het PythonPath is ${PYTHONPATH}"
+${PYTHON} setup.py install --root tmp
+
 cp -r test tmp/test
 removeDoNotDistribute tmp
 find tmp -name '*.py' -exec sed -r -e "
